326cd7a0b1079134c661a6340a1fd0ac87deea0f
Urbbiz <urbonas_andrius@yahoo.com>
2021-07-07 08:11:37 +0300

    Second commit.

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+define('INSTALL_DIR', '/home/internalandriusurbonas/php/visma-pirmas/');
+define('DIR', __DIR__ . '/');
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+require  __DIR__.'/bootstrap.php';
 
-Echo "Hello world!". '</br>';
-echo "git test";
+
+Echo "Please Enter the word you want to syllable", "\n";
+echo "Enter Word here: ";
+
+$givenWord = trim(fgets(STDIN, 1024));
