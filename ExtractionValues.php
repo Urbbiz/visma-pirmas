90b64f30026b36eac5601eb470ac794bbe6b77e1
Urbbiz <urbonas_andrius@yahoo.com>
2021-07-07 14:55:25 +0300

    Extraction class added

---
+++ ExtractionValues.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+class ExtractionValues {
+
+
+
+    // isspausdina values is txt filo
+    // reikia gautus duomenis isirasyti i masyva runtime metu.
+    public static function getValues(){
+        $values = [];
+        $file = new SplFileObject("inputfile.txt");
+        while (!$file->eof()) {
+            $values[] = $file->fgets();
+            // echo $file->fgets();
+            // echo "\n","\n";
+        }
+// echo count($values);
+// echo print_r($values);
+        return $values;
+    }
+}
+
+
+
