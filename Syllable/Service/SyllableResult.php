4e64c2696e63cb49f4154ec6f31dffb864543790
Urbbiz <urbonas_andrius@yahoo.com>
2021-07-12 12:46:32 +0300

    SyllableResul comments

--- Syllable/Service/SyllableResult.php
+++ Syllable/Service/SyllableResult.php
@@ -4,9 +4,9 @@ namespace Syllable\Service;
 
 class SyllableResult{
 
-    public string $dashResult;
-    public string $withNumbers;
-    public array $matchedPatterns;
+    public string $dashResult;  // <- zodis su bruksniais.
+    public string $withNumbers; // <- zodis su skaiciais.
+    public array $matchedPatterns; // <- skiemenys atitinkantys zodzio dalis.
 }
 
 
